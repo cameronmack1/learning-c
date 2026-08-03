81001b8ee22e0094590ff402e38df248eddae189
cameronmack1 <ckmackenzie31@gmail.com>
2026-08-03 16:06:02 -0400

    add test file for linklist

--- linklist/main.c
+++ linklist/main.c
@@ -1,9 +1,28 @@
 #include "linklist.h"
+#include <stdio.h>
 
 int main(){
+    //create 20 node
     Node* list = createNode(20);
+    printf("\ncreated list with 20\n");
+    printList(list);
+
+    //add 40 to end
     list = addToEnd(list, 40);
+    printf("\nadded 40 to end of list\n");
+    printList(list);
+
+    //add 10 to start
     list = addToStart(list, 10);
+    printf("\nadded 10 to start of list\n");
+    printList(list);
+
+    //add 30 in position 3
     list = addAtPosition(list, 30, 2);
-    list = printList(list);
+    printf("\nadded 30 to position 3\n");
+    printList(list);
+
+    list = deleteAtPosition(list, 1);
+    printf("\ndeleted position 2\n");
+    printList(list);
 }
