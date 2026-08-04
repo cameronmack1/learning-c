499dde8e7c6318165e449435507977aa7db6f412
cameronmack1 <ckmackenzie31@gmail.com>
2026-08-03 21:16:35 -0400

    started working on ncurses snake game, game loop and input handling done

---
+++ snake/snake.c
@@ -0,0 +1,106 @@
+//load curses library
+#ifdef _WIN32
+    //use pdcurses if on windows
+    #include <curses.h>
+
+    //use Sleep() on windows
+    #include <windows.h>
+    #define sleep_ms(ms) Sleep(ms);
+#else
+    //use ncurses if on linux
+    #include <ncurses.h>
+
+    //use usleep on linux
+    #include <unistd.h>
+    #define sleep_ms(ms) usleep(ms * 1000);
+#endif
+#include <time.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef enum EDirections{
+    UP,
+    LEFT,
+    DOWN,
+    RIGHT
+} Directions;
+
+int main(){
+    srand(time(NULL));
+    //init screen
+    initscr();
+    //stop typed letters from showing on terminal
+    noecho();
+    //hide terminal cursor
+    curs_set(0);
+    //enable special keys (arrows, etc)
+    keypad(stdscr, TRUE);
+    //makes input reading non-blocking so the game runs without waiting for input
+    nodelay(stdscr, TRUE);
+
+    const int FRAME_DELAY = 1000/60;
+
+    //UP, LEFT, DOWN, RIGHT, QUIT
+    bool keys[5] = {0, 0, 0, 0, 0};
+    int ch;
+
+    //game loop
+    while(1){
+        //erase last frame
+        erase();
+        //reset input array and lower the delay timer on each one
+        for(int i = 0; i < 5; i++){
+            keys[i] = 0;
+        }
+
+        ch = getch();
+        while(ch != ERR){
+            switch(ch){
+                case KEY_UP:
+                case 'W':
+                case 'w': {
+                    keys[0] = true;
+                    break;
+                }
+
+                case KEY_LEFT:
+                case 'A':
+                case 'a': {
+                    keys[1] = true;
+                    break;
+                }
+
+                case KEY_DOWN:
+                case 'S':
+                case 's': {
+                    keys[2] = true;
+                    break;
+                }
+
+                case KEY_RIGHT:
+                case 'D':
+                case 'd': {
+                    keys[3] = true;
+                    break;
+                }
+
+                case 'Q':
+                case 'q': {
+                    keys[4] = true;
+                    break;
+                }
+
+            }
+            ch = getch();
+        }
+        if(keys[4])
+            break;
+
+        refresh();
+        sleep_ms(FRAME_DELAY);
+    }
+
+
+    endwin();
+    return(0);
+}
