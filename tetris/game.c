0a22fc47224431bd696b210efaa92e4806367048
cameronmack1 <ckmackenzie31@gmail.com>
2026-08-09 00:30:52 -0400

    add logic to check if a block should lock into place

--- tetris/game.c
+++ tetris/game.c
@@ -4,19 +4,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-<<<<<<< HEAD
-#define SPAWN_X 3;
-#define SPAWN_Y -1;
-
-#define MAX_X 10;
-#define MAX_Y 20;
-=======
 #define SPAWN_X 3
 #define SPAWN_Y -1
 
 #define MAX_X 10
 #define MAX_Y 20
->>>>>>> 7a44b3f (add logic to check if a block should lock into place)
 
 void spawn_next_piece(Game* game) {
     // spawn a new piece into game->next
@@ -36,12 +28,6 @@ void to_next_piece(Game* game) {
     spawn_next_piece(game);
 }
 
-<<<<<<< HEAD
-bool check_lock(Game* game, Piece piece) {
-    // checks if a piece should lock when moving down
-}
-
-=======
 bool check_lock(Game* game, Piece* piece) {
     // checks if a piece should lock when moving down
     // loop thru pieces shape grid
@@ -66,15 +52,10 @@ bool check_lock(Game* game, Piece* piece) {
     return false;
 }
 
->>>>>>> 7a44b3f (add logic to check if a block should lock into place)
 bool check_collision(Game* game, int dx) {
     // checks if a piece can move left/right
 }
 
-<<<<<<< HEAD
-bool drop_block(Piece* piece) {
-    // returns true if the block is hitting the ground, else moves it down a block
-=======
 bool drop_block(Game* game, Piece* piece) {
     // returns true if the block is hitting the ground, else moves it down a block
     if (check_lock(game, piece))
@@ -82,7 +63,6 @@ bool drop_block(Game* game, Piece* piece) {
 
 int hard_fall_block(Game* game) {
     // returns how many blocks for the current piece to fall for it to lock
->>>>>>> 7a44b3f (add logic to check if a block should lock into place)
 }
 
 bool init(Game** out) {
@@ -116,13 +96,8 @@ void tick(Game* game, bool input[6]) {
             game->current.x += dx;
         }
     }
-<<<<<<< HEAD
-    
-    //fall 1 space per tick if fast falling, else 1 space per 20 ticks (3/sec)
-=======
 
     // fall 1 space per tick if fast falling, else 1 space per 20 ticks (3/sec)
->>>>>>> 7a44b3f (add logic to check if a block should lock into place)
     game->fall_tick = input[5] ? 1 : 20;
 
     // block dropping
